e006dd3a4e57958fbc5d5507d40f092e0d5ef17a
salma77 <salmaabdelfatah77@gmail.com>
2023-05-04 22:22:00 +0300

    added solution state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(()=> {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json => {
+      // random int between 0 to 14
+      const randomSolution = json[Math.floor(Math.random() * json.length)]
+      setSolution(randomSolution.word)
+    })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
