7abffe0040bb01ba24b6167d64e9fbad073cc938
salma77 <salmaabdelfatah77@gmail.com>
2023-05-04 22:48:13 +0300

    added handleKeyup in Wordle component

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
 
 export default function Wordle({ solution }) {
-  return <div>Wordle</div>;
+    const {currentGuess, handleKeyup} = useWordle(solution)
+    useEffect(()=>{
+        window.addEventListener("keyup", handleKeyup)
+
+        return () => window.removeEventListener("keyup", handleKeyup)
+    }, [handleKeyup])
+    return <div>Wordle</div>;
 }
