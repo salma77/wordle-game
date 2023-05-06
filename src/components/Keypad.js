77cc77e11293f9bad1e2f1f2e20e231d68217737
salma77 <salmaabdelfatah77@gmail.com>
2023-05-06 23:53:39 +0300

    added the keypad component with its styling

---
+++ src/components/Keypad.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+export default function Keypad() {
+  const [letters, setLetters] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/letters")
+      .then(res => res.json())
+      .then(json => {
+        setLetters(json);
+      });
+  }, []);
+
+  return (
+    <div className="keypad">
+      {letters &&
+        letters.map(letter => {
+          return (
+            <div key={letter.key}>
+              {letter.key}
+            </div>
+          );
+        })}
+    </div>
+  );
+}
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(
@@ -23,6 +24,7 @@ export default function Wordle({ solution }) {
         current guess - {currentGuess}{" "}
       </div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad />
     </div>
   );
 }
